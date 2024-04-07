bd1590aaa879d3199a93d6ac5fe605bd59fb935d
jgeofil <jeremy.geo@gmail.com>
2024-04-07 16:02:06 -0400

    Refactor list of posts and about page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ML Readme';
+export const SITE_DESCRIPTION = 'The latest research in machine learning and AI explained.';
