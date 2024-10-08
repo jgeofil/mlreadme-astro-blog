d3fab0232723e2e9ea432b208909d8fa212d6831
jgeofil <jeremy.geo@gmail.com>
2024-10-08 02:14:02 -0400

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,23 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import 'dotenv/config';
 import pageInsight from "astro-page-insight";
-import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
+import vercel from '@astrojs/vercel/serverless';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mlread.me',
+  adapter: vercel(),
+  output: 'hybrid',
+  server: { port: 3000, host: true },
   integrations: [
-    mdx(), 
-    sitemap(), 
-    sentry(), 
+    mdx(),
+    sitemap(),
     spotlightjs(),
     pageInsight(),
-    storyblok({
-      accessToken: process.env.STORYBLOK_TOKEN,
-      bridge: true,
-      apiOptions: {
-        region: "us",
-      },
-      components: {
-        feature: 'storyblok/Feature',
-        //grid: 'storyblok/Grid',
-        //teaser: 'storyblok/Teaser',
-      },
-    }),
+    tailwind()
   ]
 });
