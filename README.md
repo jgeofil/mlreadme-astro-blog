4598cb9d39a73f513ad29e63bbc1e6be534b5ad2
jgeofil <jeremy.geo@gmail.com>
2024-10-22 02:24:36 -0400

    feat: Configure Prettier for consistent code style
    Added Prettier configuration files to enforce a consistent code style across the project.
    
    This improves code readability and maintainability by automating formatting choices, reducing manual effort and potential conflicts arising from stylistic preferences.

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ML Readme
 
 ML Readme blog.
-
-
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mlread.me',
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
-  root: './',
-  output: 'server',
-  server: { port: 3000, host: true },
-  build: {
-
-  },
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false,
-    })
-  ]
-});
+	site: 'https://mlread.me',
+	adapter: vercel({
+		edgeMiddleware: true
+	}),
+	root: './',
+	output: 'server',
+	server: { port: 3000, host: true },
+	build: {},
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind({
+			applyBaseStyles: false
+		})
+	]
+})
