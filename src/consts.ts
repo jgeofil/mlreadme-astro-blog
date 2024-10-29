e138620172f688e97f14a4c3695b7d7d902cfb42
jgeofil <jeremy.geo@gmail.com>
2024-10-29 07:09:35 -0400

    Refactor: Improve site branding and SEO
    Updates site title and description to be more descriptive and improve SEO. Introduces the site name as a distinct branded element throughout the site. Also includes a minor visual improvement to tag hover behavior.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'ML Readme'
-export const SITE_DESCRIPTION = 'The latest research in machine learning and AI explained.'
+export const TYPEFACE = 'ML Readme'
+export const SITE_TITLE = 'ML Readme - Machine Learning and AI Blog'
+export const SITE_DESCRIPTION = 'Machine learning and AI explained: latest research and news, python (tensorflow, pytorch, keras) coding tutorials, and more.'
