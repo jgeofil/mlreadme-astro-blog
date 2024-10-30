c11f7b38c0e8d2158c66d365afb11ca95f1ba4d6
jgeofil <jeremy.geo@gmail.com>
2024-10-30 11:48:19 -0400

    Refactor: Remove theme switcher and DaisyUI
    Removed the theme switcher component and DaisyUI dependency to simplify the site and reduce bundle size.  This eliminates unused CSS and JavaScript, resulting in a slightly faster load time. The theme is now permanently set to light mode.

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,47 +52,5 @@ export default {
 				carret: '">"'
 			}
 		}
-	},
-	plugins: [
-		require('daisyui')
-	],
-	daisyui: {
-		themes: [
-			'light',
-			{black: {
-				"color-scheme": "dark",
-				"primary": "#373737",
-				"secondary": "#373737",
-				"accent": "#373737",
-				"base-100": "#000000",
-				"base-200": "#141414",
-				"base-300": "#262626",
-				"base-content": "#d6d6d6",
-				"neutral": "#373737",
-				"info": "#0000ff",
-				"success": "#008000",
-				"warning": "#ffff00",
-				"error": "#ff0000",
-				"--rounded-box": "0",
-				"--rounded-btn": "0",
-				"--rounded-badge": "0",
-				"--animation-btn": "0",
-				"--animation-input": "0",
-				"--btn-focus-scale": "1",
-				"--tab-radius": "0",
-			  },
-			},
-			  'lofi',
-			  'nord',
-			  'sunset'
-			], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: "dark", // name of one of the included themes for dark mode
-		base: true, // applies background color and foreground color for root element by default
-		styled: true, // include daisyUI colors and design decisions for all components
-		utils: true, // adds responsive and modifier utility classes
-		prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-		logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-		themeRoot: ":root", // The element that receives theme color CSS variables
-	  },
-
+	}
 }
