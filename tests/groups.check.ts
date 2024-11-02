913f0cea750beb2a3c41f526705a40f2cbc2aad4
jgeofil <jeremy.geo@gmail.com>
2024-11-01 23:36:25 -0400

    Enhance Checkly config and API checks
    Improved the reliability and coverage of Checkly checks.  Extended check locations to include `ca-central-1` and `ap-east-1`, and introduced a retry strategy with a linear backoff to handle transient failures.  API checks are now grouped and include performance assertions for response times.  The check frequency has been adjusted to optimize resource utilization.  Renamed groups and environments for clarity.  Removed the base check file and incorporated its logic directly within the group definition.

--- tests/groups.check.ts
+++ tests/groups.check.ts
@@ -1,32 +1,56 @@
 // __checks__/groups.check.ts
 import { CheckGroup } from 'checkly/constructs'
+import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
 //import { smsChannel, emailChannel } from '../alert-channels'
 //const alertChannels = [smsChannel, emailChannel]
+
+const BASE_URL = 'mlread.me'
 ​
 export const groupPreview = new CheckGroup('group-browser-dev', {
-  name: 'preview',
-  activated: true,
-  muted: false,
-  runtimeId: '2023.09',
-  locations: ['us-east-1', 'eu-west-1'],
-  tags: ['preview'],
+  name: 'preview-group',
+  tags: ['preview', 'website'],
   // You can use group-level environment vars to point each group's checks to the right target URL
   environmentVariables: [{ key: 'TARGET_URL', value: 'https://mlreadme.vercel.app' }],
-  apiCheckDefaults: {},
   concurrency: 100,
   //alertChannels
 })
 
 export const groupProd = new CheckGroup('group-browser-prod', {
-  name: 'production',
-  activated: true,
-  muted: false,
-  runtimeId: '2023.09',
-  locations: ['us-east-1', 'eu-west-1'],
-  tags: ['production'],
+  name: 'production-group',
+  tags: ['production', 'website'],
   // You can use group-level environment vars to point each group's checks to the right target URL
-  environmentVariables: [ { key: 'TARGET_URL', value: 'https://mlread.me' }],
-  apiCheckDefaults: {},
+  environmentVariables: [ { key: 'TARGET_URL', value: `https://${BASE_URL}` }],
   concurrency: 100,
   //alertChannels
+})
+
+
+new ApiCheck('base-api-check', {
+  name: 'base',
+  group:groupProd,
+  tags: ['production'],
+  maxResponseTime: 5000,
+  degradedResponseTime: 2000,
+  request: {
+    method: 'GET',
+    url: `https://${BASE_URL}`,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200)
+    ],
+  }
+})
+
+new ApiCheck('base-www-api-check', {
+  name: 'base-www',
+  group: groupProd,
+  tags: ['www', 'production'],
+  maxResponseTime: 5000,
+  degradedResponseTime: 2000,
+  request: {
+    method: 'GET',
+    url: `https://www.${BASE_URL}`,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200)
+    ],
+  }
 })
