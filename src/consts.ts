81c640bc0c1bc6283803845257dfc6bd69d21eeb
jgeofil <jeremy.geo@gmail.com>
2024-11-04 15:51:49 -0500

    Refactor: Enhance content and remove unused integrations
    - Removed unused Spotlight.js and Page Insight integrations to streamline the project.
    - Improved SEO and social sharing by integrating `fetch-meta-tags` to dynamically fetch metadata from linked URLs.
    - Enhanced the `Bookmark` component to display richer previews, including title, description, icon, and image, improving user experience.
    - Updated `PostCard` and `global` styles for better content presentation.
    - Improved website SEO with updated metadata and landing page copy.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ML Readme';
-export const SITE_DESCRIPTION = 'The latest research in machine learning and AI explained.';
+export const SITE_TITLE = 'Machine Learning Tutorials | Python Coding & AI Articles | ML Readme';
+export const SITE_DESCRIPTION = 'Discover in-depth, step-by-step machine learning tutorials and AI articles, covering Python coding, research summaries, and practical examples. Empower your learning journey with ML Readme.';
