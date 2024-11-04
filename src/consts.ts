f56491d50ad2b79eae7f8672607d943742fcdf95
jgeofil <jeremy.geo@gmail.com>
2024-11-04 16:02:16 -0500

    Shorten site title in header
    Use the shorter "ML Readme" title in the header for brevity and visual appeal.  Also added `SITE_URL` constant.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
+export const SITE_URL = 'https://mlread.me';
+export const SITE_TITLE_SHORT = 'ML Readme';
 export const SITE_TITLE = 'Machine Learning Tutorials | Python Coding & AI Articles | ML Readme';
 export const SITE_DESCRIPTION = 'Discover in-depth, step-by-step machine learning tutorials and AI articles, covering Python coding, research summaries, and practical examples. Empower your learning journey with ML Readme.';
