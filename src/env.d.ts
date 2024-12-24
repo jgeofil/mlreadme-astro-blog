19df955f52010a378fbf1a07ed74b58d08fea96b
jgeofil <jeremy.geo@gmail.com>
2024-12-24 02:49:21 -0500

    Updates Supabase key usage
    Changes the Supabase key being used from the generic `SUPABASE_KEY` to `SUPABASE_AUTH_ANON_KEY`.
    
    This allows for proper usage of the anon key specifically, rather than the potential service key
    or the url for the supabase.
    
    Adds more Supabase related env vars for later use.

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,6 +7,10 @@ interface ImportMetaEnv {
 	readonly PUBLIC_TAGGIN_SERVER?: string // your example
 	readonly SUPABASE_URL: string
 	readonly SUPABASE_KEY: string
+	readonly SUPABASE_DB_URL: string
+	readonly SUPABASE_AUTH_SERVICE_ROLE_KEY: string
+	readonly SUPABASE_AUTH_JWT_SECRET: string
+	readonly SUPABASE_AUTH_ANON_KEY: string
 }
 
 interface ImportMeta {
